f13fa201e6632ded51554346959b2a9663cd9812
Kushal <kushalsinha.001@gmail.com>
2025-05-03 19:26:45 +0530

    My second program

---
+++ Acccessspecifier1.java
@@ -0,0 +1,22 @@
+package Module2;
+
+public class Acccessspecifier1 
+{
+	
+	public static void add()
+	{
+		
+	}
+	protected static void sub()
+	{
+		
+	}
+	private static void mul()
+	{
+		
+	}
+	static void div()
+	{
+		
+	}
+}
